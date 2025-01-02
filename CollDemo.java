2ee2d1ac42d1d17ad4b1b4447311d102eb0cc7df
Jagadish Hegade <42802228+J3-Aquacool@users.noreply.github.com>
2025-01-02 15:59:07 +0530

    Add files via upload

---
+++ CollDemo.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class CollDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList <Integer>al = new ArrayList();
+		al.add(10);
+		al.add(40);
+		al.add(50);
+		al.add(60);
+		al.add(80);
+		
+		al.add("Java");
+		System.out.println("Array List:" + al);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ IteratorDemo.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class IteratorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList <Integer> al = new ArrayList<>();
+		al.add(10);
+		al.add(40);
+		al.add(50);
+		al.add(60);
+		al.add(80);
+		al.add("sss"); // Type-Safe
+		
+		System.out.println("Array List:" + al);
+
+		 System.out.println("Using enchnced for"); 
+		  for(Object o:al)
+		  {
+		  System.out.println(o); 
+		  //al.add(70);
+		  
+		  } 
+		  
+		  
+			
+			 /* System.out.println("Using regular for");
+			  
+			  for(int i=0;i<al.size();i++) 
+			  {
+			  
+			  
+			  System.out.println(al.get(i)); 
+			  al.add(990);
+			  
+			  }
+			  */
+		  
+	
+			  
+			  System.out.println("Using iterator");
+			  	  
+			  Iterator it=al.iterator();
+			  
+			  while(it.hasNext()) {
+				  //Integer str=(Integer)it.next(); 
+				  		  System.out.println(it.next());
+			 
+			  
+			  
+			 }
+			  
+			  
+System.out.println("Using List Iterator");
+
+ListIterator listiterate= al.listIterator();
+
+
+while(listiterate.hasNext())
+{
+	System.out.println(listiterate.next());
+}
+
+System.out.println("Using List Iterator in reverse orer");
+
+while(listiterate.hasPrevious())
+{
+	System.out.println(listiterate.previous());
+}
+
+
+
+
+	}
+
+}
